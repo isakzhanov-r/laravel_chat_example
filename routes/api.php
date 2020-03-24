a37565424bb15cdbd29fc28ecf33391bb51c983e
isakzhanovr <isakzhanov.r@gmail.com>
2020-03-24 17:20:16 +0300

    рефакторинг роутов

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+app('router')
+    ->post('/login', 'Auth\LoginController@login')
+    ->name('login');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+app('router')
+    ->post('/register', 'Auth\RegisterController@register')
+    ->name('register');
+
+app('router')
+    ->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')
+    ->name('password.email');
+
+app('router')
+    ->post('/password/reset', 'Auth\ResetPasswordController@reset')
+    ->name('password.update');
+
+app('router')
+    ->middleware('auth')
+    ->group(\base_path('routes/api/_auth.php'));
+
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-app('router')
-    ->name('api.')
-    ->prefix('api')
-    ->group(\base_path('routes/web/_api.php'));
 
 app('router')
     ->get('/{path?}', 'IndexController')
